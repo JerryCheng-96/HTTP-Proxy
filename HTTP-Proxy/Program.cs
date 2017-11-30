7279c4db2eb81de929dc0e3850122dcfeb033575
JerryCheng <jerrycheng96@live.com>
2017-11-30 15:31:47 +0800

    Add project files.

---
+++ HTTP-Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTTP_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
